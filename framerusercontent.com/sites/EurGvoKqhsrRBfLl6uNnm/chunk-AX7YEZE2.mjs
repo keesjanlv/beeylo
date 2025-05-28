0da3e77fc26180f254248f8e68071395a28e5948
keesjanlv <keesjanvg@gmail.com>
2025-05-28 19:19:04 +0200

    Initial commit

---
+++ framerusercontent.com/sites/EurGvoKqhsrRBfLl6uNnm/chunk-AX7YEZE2.mjs
@@ -0,0 +1,2 @@
+function t(e,a){return{description:"Beeylo",favicon:"https://framerusercontent.com/assets/f4O9Q3jrjk8Jj4OeKlllKuhAwY.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/MI6Wt0acUM5GB9R6Y9VNGQ5Azc.png",title:"Beeylo"}}export{t as a};
+//# sourceMappingURL=chunk-AX7YEZE2.mjs.map
