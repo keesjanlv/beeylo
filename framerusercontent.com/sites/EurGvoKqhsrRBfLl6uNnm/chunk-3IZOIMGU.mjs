2b7ca59e6424e22b572948ae65f5477c111989cc
keesjanlv <keesjanvg@gmail.com>
2025-05-29 16:16:01 +0200

    Update

---
+++ framerusercontent.com/sites/EurGvoKqhsrRBfLl6uNnm/chunk-3IZOIMGU.mjs
@@ -0,0 +1,2 @@
+import{f as t}from"./chunk-G3CAO4HR.mjs";t();function e(a,r){return{description:"Beeylo",favicon:"https://framerusercontent.com/assets/f4O9Q3jrjk8Jj4OeKlllKuhAwY.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/MI6Wt0acUM5GB9R6Y9VNGQ5Azc.png",title:"Beeylo"}}export{e as a};
+//# sourceMappingURL=chunk-3IZOIMGU.mjs.map
---
+++ framerusercontent.com/sites/EurGvoKqhsrRBfLl6uNnm/chunk-U53FLAFU.mjs
@@ -0,0 +1,2 @@
+import{f as e}from"./chunk-G3CAO4HR.mjs";e();
+//# sourceMappingURL=chunk-U53FLAFU.mjs.map
