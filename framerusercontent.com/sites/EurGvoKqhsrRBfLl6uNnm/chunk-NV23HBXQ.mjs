169c9d0347042d91753c87e38b61c50b5607228b
keesjanlv <keesjanvg@gmail.com>
2025-06-19 09:53:03 +0200

    Update

---
+++ framerusercontent.com/sites/EurGvoKqhsrRBfLl6uNnm/chunk-NV23HBXQ.mjs
@@ -0,0 +1,2 @@
+function a(e,t){return{description:"Beeylo",favicon:"https://framerusercontent.com/assets/MWH7lZrSeqREQC3zLL6a1HDKwKE.svg",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/5EB01nkLGJzXqR3VxRGabn358M.png",title:"Beeylo"}}export{a};
+//# sourceMappingURL=chunk-NV23HBXQ.mjs.map
